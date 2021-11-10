a06ef7865662884de702a8576542ac7a75c4e063
Defqon7 <49489852+Defqon7@users.noreply.github.com>
2021-11-09 17:58:32 -0800

    Created validation set, added image augmentation, trained model

--- main.py
+++ main.py
@@ -1,16 +1,111 @@
-# This is a sample Python script.
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import os
+import random
+from shutil import copyfile
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import pickle
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+try:
+    os.mkdir('C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd')
+    os.mkdir('C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Training')
+    os.mkdir('C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Testing')
+    os.mkdir('C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Training/Dogs')
+    os.mkdir('C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Training/Cats')
+    os.mkdir('C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Testing/Dogs')
+    os.mkdir('C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Testing/Cats')
+except OSError:
+    pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def split_data(SOURCE, TRAINING, TESTING, SPLIT_SIZE):
+    files = []
+    for filename in os.listdir(SOURCE):
+        file = SOURCE + filename
+        if os.path.getsize(file) > 0:
+            files.append(filename)
+        else:
+            print(filename + " has a length of zero.")
+
+        training_length = int(len(files) * SPLIT_SIZE)
+        testing_length = int(len(files) - training_length)
+        shuffled_set = random.sample(files, len(files))  # shuffle the files
+        training_set = shuffled_set[0:training_length]  # from index 0 to training_length
+        testing_set = shuffled_set[-testing_length:]  # rest of files
+
+    for filename in training_set:
+        this_file = SOURCE + filename
+        destination = TRAINING + filename
+        copyfile(this_file, destination)
+
+    for filename in testing_set:
+        this_file = SOURCE + filename
+        destination = TESTING + filename
+        copyfile(this_file, destination)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+CAT_SOURCE_DIR = 'C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/Cat/'
+TRAINING_CATS_DIR = 'C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Training/Cats/'
+TESTING_CATS_DIR = 'C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Testing/Cats/'
+DOG_SOURCE_DIR = 'C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/Dog/'
+TRAINING_DOGS_DIR = 'C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Training/Dogs/'
+TESTING_DOGS_DIR = 'C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Testing/Dogs/'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if not os.path.exists('C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd'):
+    split_size = .9
+    split_data(CAT_SOURCE_DIR, TRAINING_CATS_DIR, TESTING_CATS_DIR, split_size)
+    split_data(DOG_SOURCE_DIR, TRAINING_DOGS_DIR, TESTING_DOGS_DIR, split_size)
+
+print(len(os.listdir('C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Training/Cats')))
+print(len(os.listdir('C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Training/Dogs')))
+print(len(os.listdir('C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Testing/Cats')))
+print(len(os.listdir('C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Testing/Dogs')))
+
+# model
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+# compile model
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(learning_rate=.001),
+              metrics=['acc'])
+
+# pre process images
+TRAINING_DIR = 'C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Training/'
+train_datagen = ImageDataGenerator(rescale=1.0 / 255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+train_generator = train_datagen.flow_from_directory(TRAINING_DIR,
+                                                    target_size=(150, 150),
+                                                    batch_size=250,
+                                                    class_mode='binary')
+VALIDATION_DIR = 'C:/Users/hanna/OneDrive/Desktop/Datasets/PetImages/cvd/Testing/'
+validation_datagen = ImageDataGenerator(rescale=1.0 / 255.)
+validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
+                                                              target_size=(150, 150),
+                                                              batch_size=250,
+                                                              class_mode='binary')
+
+history = model.fit(train_generator, epochs=20, steps_per_epoch=90,
+                    validation_data=validation_generator, validation_steps=6)
+
+# save model
+model.save('models/cat_or_dog.h5')
